60cd5fda88f498941628a182b38fce893ea13b61
vedantyerne1-art <vedantyerne1@gmail.com>
2026-07-06 19:57:11 +0530

    Time: 1 ms (15.7%), Space: 44.7 MB (18.84%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+
+        int maxwealth=0;
+
+        for(int i=0;i<accounts.length;i++){
+
+            int wealth=0;
+
+            for(int j=0;j<accounts[i].length;j++){
+                wealth=wealth+accounts[i][j];
+            }
+            maxwealth=Math.max(maxwealth,wealth);
+        }
+
+        return maxwealth;
+        
+    }
+}
