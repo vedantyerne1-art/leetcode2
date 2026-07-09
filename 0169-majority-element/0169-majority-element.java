1d1675182376f33ca2783caa2e0dc82dff0ccfd7
vedantyerne1-art <vedantyerne1@gmail.com>
2026-07-09 20:02:57 +0530

    Time: 1 ms (99.9%), Space: 56 MB (6.53%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,13 +1,13 @@
 class Solution {
     public int majorityElement(int[] nums) {
-
-        int count =0 ;
+        int count=0;
         int number=0;
 
         for(int i=0;i<nums.length;i++){
             if(count==0){
                 number=nums[i];
             }
+
             if(nums[i]==number){
                 count++;
             }
@@ -15,8 +15,8 @@ class Solution {
             else{
                 count--;
             }
+            
         }
-
         return number;
         
     }
