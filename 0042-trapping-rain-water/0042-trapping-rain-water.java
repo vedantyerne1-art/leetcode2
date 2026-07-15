0ace1047f97fb0769ec8acb28f4ad43a932ae5e8
Vedant Yerne <vedantyerne1@gmail.com>
2026-07-15 20:30:05 +0530

    Time: 1 ms (60.6%), Space: 47.6 MB (81.78%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int trap(int[] height) {
+        int left=0;
+         int right=height.length-1;
+
+         int leftmax=0;
+         int rightmax=0;
+
+         int water =0;
+
+         while(left<right){
+            if(height[left]<height[right]){
+                if(height[left]>=leftmax){
+                    leftmax=height[left];
+                }
+                else{
+                    water=water +leftmax-height[left];
+                }
+                left++;
+            }
+
+            else{
+                if(height[right]>=rightmax){
+                    rightmax=height[right];
+                }
+                else{
+                    water=water+rightmax-height[right];
+                }
+                right--;
+            }
+         }
+
+         return water ;
+        
+    }
+}
